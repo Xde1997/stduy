c3299da2f14d1487bd44669b2b295a6aa3d2c784
xde1997 <861529206@qq.com>
2021-09-11 18:50:29 +0800

    入库golang相关代码

---
+++ go.mod
@@ -0,0 +1,8 @@
+module main
+
+go 1.15
+
+require (
+	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394 // indirect
+	github.com/kardianos/service v1.2.0
+)
---
+++ runtime/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	Foo()
+}
+func Foo() {
+	fmt.Printf("我是 %s, %s 在调用我!\n", printMyName(), printCallerName())
+	Bar()
+}
+func Bar() {
+	fmt.Printf("我是 %s, %s 又在调用我!\n", printMyName(), printCallerName())
+}
+func printMyName() string {
+	pc, _, _, _ := runtime.Caller(1)
+	return runtime.FuncForPC(pc).Name()
+}
+func printCallerName() string {
+	pc, _, _, _ := runtime.Caller(2)
+	return runtime.FuncForPC(pc).Name()
+}
