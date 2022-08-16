6357f37a9cbfa00016679d4cce56cdd580e99fcf
xdp <xdp@caxa.com>
2022-08-16 23:11:25 +0800

    1

---
+++ rust/os/target/riscv64gc-unknown-none-elf/debug/deps/os-349edaceffae7c05.d
@@ -0,0 +1,6 @@
+D:\demo\rust\rust\os\target\riscv64gc-unknown-none-elf\debug\deps\os-349edaceffae7c05: src\main.rs src\lang_items.rs
+
+D:\demo\rust\rust\os\target\riscv64gc-unknown-none-elf\debug\deps\os-349edaceffae7c05.d: src\main.rs src\lang_items.rs
+
+src\main.rs:
+src\lang_items.rs:
---
+++ rust/os/target/riscv64gc-unknown-none-elf/debug/os.d
@@ -0,0 +1,1 @@
+D:\demo\rust\rust\os\target\riscv64gc-unknown-none-elf\debug\os: D:\demo\rust\rust\os\src\lang_items.rs D:\demo\rust\rust\os\src\main.rs
